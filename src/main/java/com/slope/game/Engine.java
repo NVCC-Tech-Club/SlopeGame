97bfec06256354066b53aa1f343c630c159f662c
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-10-04 02:39:35 -0400

    Comments

--- src/main/java/com/slope/game/Engine.java
+++ src/main/java/com/slope/game/Engine.java
@@ -3,6 +3,7 @@ package com.slope.game;
 import org.lwjgl.opengl.GL21;
 import org.lwjgl.glfw.GLFW;
 
+// This class is responsible for the main loop of the game, physics, rendering, etc.
 class Engine {
     public static void begin() {
         GL21.glClear(GL21.GL_COLOR_BUFFER_BIT | GL21.GL_DEPTH_BUFFER_BIT);
