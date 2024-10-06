2deaa6157ae7986ae7bfba4a8f0522db27020169
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-10-06 17:34:36 -0400

    Yup

--- src/main/java/com/slope/game/Game.java
+++ src/main/java/com/slope/game/Game.java
@@ -4,6 +4,9 @@ public class Game extends Core {
     private static final int MAX_PlATFORM_CAPACITY = 10;
 
     // TODO: Also add multiple Fixed Sized Queues here for platforms and red block pillars. (Feeshy Task Only)
+    // NOTE: I will have to create my own Fixed Size Queue class that uses LWJGL's direct memory components to
+    // allocate and deallocate my capacity and map memory to it like I was in C.
+
     // TODO: Probably have an ArrayList for the red obstacles in every platform. (Feeshy Task Only)
 
     public Game() {
