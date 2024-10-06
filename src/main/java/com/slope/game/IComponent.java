99313bcc9deb7ba6d4863a5f013080eb67f7af47
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-10-06 17:20:52 -0400

    Cleanup

--- src/main/java/com/slope/game/IComponent.java
+++ src/main/java/com/slope/game/IComponent.java
@@ -1,15 +1,2 @@
-package com.slope.game;
-
-public interface IComponent {
-    // Initialize our component.
-    void init();
-
-    // Render our component.
-    void render();
-
-    // Update our component.
-    void update();
-
-    // Destroy our component.
-    void destroy();
+package com.slope.game;public interface IComponent {
 }
