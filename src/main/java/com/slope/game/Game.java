625a4bb96bf57a2b377296a99f0497ed2cc02639
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-10-06 17:21:58 -0400

    Update Game.java

--- src/main/java/com/slope/game/Game.java
+++ src/main/java/com/slope/game/Game.java
@@ -3,7 +3,7 @@ package com.slope.game;
 public class Game extends Core {
     private static final int MAX_PlATFORM_CAPACITY = 10;
 
-    // TODO: Also add multiple BaseObjectPools here for platforms and red block pillars. (Feeshy Task Onlu)
+    // TODO: Also add multiple BaseObjectPools here for platforms and red block pillars. (Feeshy Task Only)
     // TODO: Probably have an ArrayList for the red obstacles in every platform. (Feeshy Task Only)
 
     public Game() {
@@ -12,7 +12,7 @@ public class Game extends Core {
 
     @Override
     public void init() {
-        // TODO: Add stuff above our pre-init to it can get loaded to the renderer. (Feeshy Task Onlu)
+        // TODO: Add stuff above our pre-init to it can get loaded to the renderer. (Feeshy Task Only)
 
         super.init();
     }
