888a062eb6db2080ae36d85fb88703d9ecd9b4ce
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-10-06 17:30:08 -0400

    Rename to Fixed Size Queues

--- src/main/java/com/slope/game/Game.java
+++ src/main/java/com/slope/game/Game.java
@@ -3,7 +3,7 @@ package com.slope.game;
 public class Game extends Core {
     private static final int MAX_PlATFORM_CAPACITY = 10;
 
-    // TODO: Also add multiple BaseObjectPools here for platforms and red block pillars. (Feeshy Task Only)
+    // TODO: Also add multiple Fixed Sized Queues here for platforms and red block pillars. (Feeshy Task Only)
     // TODO: Probably have an ArrayList for the red obstacles in every platform. (Feeshy Task Only)
 
     public Game() {
