b52c4f2cbcb87b8038ed7a2d703fb554099dcebf
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-10-09 21:44:51 -0400

    Update Shaders

--- src/main/resources/shaders/include/camera.glsl
+++
@@ -1,8 +0,0 @@
-layout(std140) uniform CameraMatrices {
-    mat4 projectionMatrix;
-    mat4 viewMatrix;
-    mat3 rotationMatrix;
-    vec3 position;
-    float nearPlane;
-    float farPlane;
-} CamMatrix;
--- src/main/resources/shaders/main-vertex.glsl
+++ src/main/resources/shaders/main-vertex.glsl
@@ -1,9 +1,18 @@
 #version 410 core
 
+layout(std140) uniform CameraMatrices {
+    mat4 projectionMatrix;
+    mat4 viewMatrix;
+    mat3 rotationMatrix;
+    vec3 position;
+    float nearPlane;
+    float farPlane;
+} CamMatrix;
+
 in vec3 pos;
 out vec3 color;
 
 void main() {
-    gl_Position = vec4(pos, 1.0);
+    gl_Position = CamMatrix.projectionMatrix * vec4(pos, 1.0);
     color = vec3(pos.x + 0.8, 0.8, pos.y + 0.8);
 }
