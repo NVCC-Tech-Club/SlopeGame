0267399157d20d444cb44bc97da407dde8f08f4b
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-10-24 14:36:38 -0400

    Unbind textures to increase performance

--- src/main/java/com/slope/game/FrameBuffer.java
+++ src/main/java/com/slope/game/FrameBuffer.java
@@ -1,6 +1,7 @@
 package com.slope.game;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL21;
 import org.lwjgl.opengl.GL30;
 
 import java.nio.ByteBuffer;
@@ -9,10 +10,12 @@ public class FrameBuffer {
     private int fbo;
     private int rbo;
     private int texture;
+    private int depthTexture;
 
     public FrameBuffer() {
         fbo = 0;
         texture = 0;
+        depthTexture = 0;
     }
 
     public void init(int width, int height) {
@@ -24,12 +27,17 @@ public class FrameBuffer {
         // Create texture to attach to FBO
         texture = GL11.glGenTextures();
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture);
-        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, (ByteBuffer) null);
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, width, height, 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, (ByteBuffer) null);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
 
         // Attach texture to FBO
         GL30.glFramebufferTexture2D(GL30.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, GL11.GL_TEXTURE_2D, texture, 0);
+
+        // Create texture to attach to RBO
+        depthTexture = GL11.glGenTextures();
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, depthTexture);
+        
 
         // Check if FBO is complete
         //GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, fbo);
@@ -52,6 +60,7 @@ public class FrameBuffer {
 
     public void bind() {
         GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, fbo);
+        GL21.glBindTexture(GL21.GL_TEXTURE_2D, 0);
     }
 
     public void destroy() {
