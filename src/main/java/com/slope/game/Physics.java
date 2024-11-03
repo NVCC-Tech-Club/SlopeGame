5176d4ef9dfb77db6d5cfaa1bec17b25ace0f88e
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-11-03 05:20:18 -0500

    Update Physics.java

--- src/main/java/com/slope/game/Physics.java
+++ src/main/java/com/slope/game/Physics.java
@@ -4,11 +4,12 @@ import org.joml.Vector3f;
 
 public class Physics {
     private static final Vector3f c = new Vector3f(0.0f, 5.0f, 0.0f);
-    private static final float GRAVITY_STRENGTH = -0.1f;
+    private static final float GRAVITY_STRENGTH = -9.8f;
     private static Vector3f sphere_pos = new Vector3f(0.0f, 0.0f, 0.0f);
 
     public static Vector3f getPosition() { // Gravity Pull
-        sphere_pos.set(sphere_pos.x, sphere_pos.y + GRAVITY_STRENGTH * Engine.getMain().getITime(), sphere_pos.z);
+        float gravity = 0.5f * GRAVITY_STRENGTH * Engine.getMain().getITime() * Engine.getMain().getITime();
+        sphere_pos.set(sphere_pos.x, sphere_pos.y + gravity, sphere_pos.z);
         //System.out.println(sphere_pos.y);
         return sphere_pos;
     }
